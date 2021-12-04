1c1c6f720152d648fa0d68e44eadd241d080d609
IanT187 <95546373+IanT187@users.noreply.github.com>
2021-12-04 21:24:17 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+#example of multi-line printing, where you can print wysiwyg style
+
+print ('''
+
+==========
+|        |
+| A Box  |
+|        |
+==========
+
+''')
+
+for i in range (2,5):
+    print (i)
+
+x = 'silent'
+print (x[2] + x[1] + x[0] + x[5] + x[3] + x[4])
+
+
